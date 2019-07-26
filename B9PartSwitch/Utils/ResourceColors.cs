5769d52b5c3b5b1a98dd3ae6ab94a0818e10d808
blowfish <joeydwong@gmail.com>
2019-07-25 18:05:18 -0700

    replace custom resource colors with xkcd colors

--- B9PartSwitch/Utils/ResourceColors.cs
+++ B9PartSwitch/Utils/ResourceColors.cs
@@ -5,11 +5,11 @@ namespace B9PartSwitch.Utils
 {
     public static class ResourceColors
     {
-        public static Color LiquidFuel = new Color(1, 0.75f, 0.5f);
-        public static Color LqdHydrogen = new Color(0.75f, 0.75f, 1);
-        public static Color Oxidizer = new Color(0.25f, 0.25f, 1);
-        public static Color MonoPropellant = new Color(1, 1, 0.5f);
-        public static Color ElectricChargePrimary = new Color(0.125f, 0.125f, 0.125f);
-        public static Color ElectricChargeSecondary = new Color(1, 1, 0.375f);
+        public static Color LiquidFuel = XKCDColors.Peach;
+        public static Color LqdHydrogen = XKCDColors.LightPeriwinkle;
+        public static Color Oxidizer = XKCDColors.LightRoyalBlue;
+        public static Color MonoPropellant = XKCDColors.FadedYellow;
+        public static Color ElectricChargePrimary = XKCDColors.Dark;
+        public static Color ElectricChargeSecondary = XKCDColors.Canary;
     }
 }
