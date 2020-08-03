abbee6b7e40940372f6dca74b46e2c7b707dc92b
Lisias <me@lisias.net>
2020-08-03 19:00:29 -0300

    Adding Startup

---
+++ Source/B9PartSwitch/Startup.cs
@@ -0,0 +1,41 @@
+﻿/*
+		This file is part of B9PartSwitch /L Unofficial
+		© 2020 LisiasT
+
+		B9PartSwitch /L Unofficial is licensed as follows:
+
+		* LGPL 3.0 : https://www.gnu.org/licenses/lgpl-3.0.txt
+
+		B9PartSwitch /L Unofficial is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+		You should have received a copy of the GNU Lesser General Public License 3.0
+		along with B9PartSwitch /L Unofficial. If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+
+using UnityEngine;
+
+namespace B9PartSwitch
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class Startup : MonoBehaviour
+	{
+        private void Start()
+        {
+            Log.init();
+            Log.force("Version {0}", Version.Text);
+
+            try
+            {
+                KSPe.Util.Installation.Check<Startup>(typeof(Version));
+            }
+            catch (KSPe.Util.InstallmentException e)
+            {
+                Log.error(e.ToShortMessage());
+                KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+            }
+        }
+	}
+}
