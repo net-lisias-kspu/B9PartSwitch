8298e01644e30f68f0d12beea2fb5805a59f59dd
Lisias <me@lisias.net>
2020-08-03 18:28:50 -0300

    Project Normalizing

---
+++ Source/B9PartSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("B9PartSwitch")]
+[assembly: AssemblyDescription("A part switching module for KSP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("L Aerospace/KSP Division")]
+[assembly: AssemblyProduct("B9PartSwitch /L Unofficial")]
+[assembly: AssemblyCopyright("© 2020 LisiasT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("011a01e9-0074-47d5-82f8-4259d3d927b7")]
+
+//[assembly: AssemblyInformationalVersionAttribute("<%= git_version %>")]
+[assembly: AssemblyVersion(B9PartSwitch.Version.Number)]
+[assembly: AssemblyFileVersion(B9PartSwitch.Version.Number)]
+
+[assembly: KSPAssembly("B9PartSwitch", B9PartSwitch.Version.major, B9PartSwitch.Version.minor, B9PartSwitch.Version.patch)]
